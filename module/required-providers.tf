39c7fbd3f89a00f0e9fc495c77e192fc1af95c8d
Carlos Angulo Mascarell <97619732+cangulomascarell@users.noreply.github.com>
2022-06-08 11:34:58 +0200

    fix: CRS-9 updated required-providers.tf to version >= 4.6.0

--- module/required-providers.tf
+++ module/required-providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "<= 3.73.0" # <= https://github.com/hashicorp/terraform-provider-aws/issues/22866
+      version               = ">= 4.6.0"
       configuration_aliases = [aws.nwk, aws.domain]
     }
   }
